cd2760536c6b20ff9be02aafe6af1e051f825703
Larissa Amaral <larissa95.amaral@gmail.com>
2020-11-10 07:33:56 -0300

    inclui CORS

--- src/controller/usuario-controller.js
+++ src/controller/usuario-controller.js
@@ -1,28 +1,28 @@
-const db = require("../config/mysql").promise();
+const db = require("../config/mysql");
 
 exports.insert = async (req, res, next) => {
 
-  if (!req.body.nome || !req.body.email) {
+  if (!req.body.username) { // || !req.body.email) {
     return res.status(400).json({
       message: "Campos obrigatórios não preenchidos.",
-      fields: ["nome", "email"],
+      fields: ["username", "email"],
     });
   }
 
+  req.body.email = req.body.email ? req.body.email : null;
+
   try {
       
-    const [rows] = await db.execute(
-      "INSERT INTO `usuario`(`nome`,`email`) VALUES(?, ?)",
-      [req.body.nome, req.body.email]
-    );
-
-    if (rows.affectedRows === 1) {
-      return res.status(201).json({
-        message: "Usuário inserido com sucesso.",
-        userID: rows.insertId,
-      });
-    }
-
+    db.query(
+      "INSERT INTO `usuario`(`username`,`email`) VALUES(?, ?)",
+      [req.body.username, req.body.email],
+      (error, results, fields) => {
+        if(error) throw error;
+        res.status(201).json({
+          message: "Usuário inserido com sucesso.",
+          data: results,
+        });
+    });
   } catch (err) {
     next(err);
   }
@@ -31,10 +31,10 @@ exports.insert = async (req, res, next) => {
 
 exports.selectAllUsers = async (req, res, next) => {
   try {
-
-    const [rows] = await db.execute("SELECT * FROM `usuario`");
-    res.status(200).json(rows);
-
+    db.query("SELECT * FROM `usuario`", (error, results, fields) => {
+      if(error) throw error;
+      res.status(200).json(results);
+    });
   } catch (err) {
     next(err);
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// O Heroku não irá utilizar o dotenv, então incluímos para usar só localmente 
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const cors = require('cors')
 
 const port = process.env.PORT || 3003
 const server = express()
@@ -13,9 +13,10 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
+server.use(cors())
+
 server.listen(port, () => { console.log(`BACKEND is running on port ${port}.` )})
 
 require('./config/mongodb')
-require('./config/mysql')
 require('./config/routes')(server)
 
